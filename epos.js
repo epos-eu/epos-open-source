e00b52fe4fcd68d5dfbfc9610b661d46c78b230c
valeriovinciarelli <Roma?=10>
2023-04-27 17:25:21 +0200

    Update EPOS code

--- epos.js
+++ epos.js
@@ -12,23 +12,37 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `EPOS Open Source
 
-                &&&&&&&&&&&&&&&&&& *&&&&&&&%&&&%               *****************               &&&&&&/         
-                &&&&&&&&&&&&&&&&&& *&&&&&&&&&&&&&&&&&       **  **********  *******       &&&&&&&&&&&&&&&&&    
-                &&&&&&&&&&&%&&&&&& *&&&&&&&%    &&&&&&&   ,************     *********    &&%&&&&&&&&&&&&&      
-                &&&&&&             *&&&&&&        &&&&&( ************   **   ********** &&&&&&#                
-                &&&&&&             *&&&&&&(       &&&&& ****** * *****  **  *********** &&&&&&&&#              
-                &&&&&&&&&&&&&&&&.  *&&&&&&&&&&&&&&&&&&& *******   *   , *    *********** &&&&&&&&&&&&&&&&      
-                &&&%&&&&&&&%&&&&.  *&&&&&&&%&&&&&&&%&   *******                 ,*******    &&&&&&&%&&&&&&&    
-                &&&&&&             *&&&&&&               *                   , ********              &&&&&&.   
-                &&&&&&             *&&&&&&               .    ******  *,    ******* **    &&         &&&&&&    
-                &&&&&&&&&&&&&&&&&& *&&&&&&                 ************** *         *   &&&&&&&&&&&&&&&&&&&    
-                &&&&&&&&&&&%&&&&&& *&&&&&&                   ************* ,*******     &&&%&&&&&&&&&&&&       
-                                                                **************                             
-  Copyright (C) 2023  EPOS ERIC
-`;
-  var speed = 5;
+  var txt = `EPOS Open Source
+
+                                                                                                        public class Developer {
+    private int numberOfCoffees; private   Boolean needCoffee; pri                                 vate int concentration; private i                              nt numberOfB
+   ugFixed; private final String OPEN =   "GOOGLE"; private static Logger                      LOG = Logger.getGlobal(); public Develope                   r() { this.concentration =
+    100; this.numberOfCoffees = 0; this.  numberOfBugFixed = 0; this.needCoffe               e =     false; } public voi    d startHackath              onRoutine() { while(LocalTime.now
+   ()!=LocalTime.parse("18:00")) { if(Lo  calTime.now()!=LocalTime.parse("11:00"          )) { checkSTREET();  drin k      Coffee(); } if(Lo          calTime.now()!=LocalTime.parse("13
+   :00")) eatSomething(); else doBugfix(  ); LOG.warning("A c     offee is necess        ary now"); drinkCoffee();         } LOG.warning("Is t       his the end of the Hackathon day?
+   "); LOG.info(                          "Before going ba            ck home do:"     ); doSomethingRelaxing();        }    private void checkS    TREET() { check           VIEW(
+   ); } private                           void checkVIEW               () { LOG.inf   o("48.3732367,-123.5868        78      8"); } private void   eatSomething()
+    { drinkCoffe                          e(); } private                void doBugf  ix() { LOG.inf  o("This         co    de will not be fixed by  itself, let's
+    go!!!!!"); t                          his.setConcent               ration(this. getConcentr a ti on()-1); th    is.    setNumberOfBugFixed(thi  s.getNumberOfBu
+   gFixed()+1); if(this.getConcentrat     ion()<50) this.n           eedCoffee = t  rue; } pri   v a  te void  d   rin     kCoffee() { this.setConc entration(100); this.needCoff
+   ee = false; } private void doSomet     hingRelaxing() { Ran   dom randomNumber    = new Random(     ); int  n = ra        ndomNumber.nextInt();   Set<Integer> primeNumbersSet = n
+   ew HashSet<Integer> (); for(int si     eve=2 ; sieve <=n ; sieve++){ primeNum    bersSet.add(sieve)                        ; } for(Integer of :     primeNumbersSet){ for(int m = 2*o
+   f ; m <= n ; m += of) primeNumbers     Set.remove(m); } for(Integer p : pri      meNumbersSet)                                  LOG.info(p + " "        ); LOG.info("ConsidERATiOn abo
+   ut SomeTHing to drink before ENdin     g thE Stressing day"); greetings(         ); } public S                                     tring greetin              gs() { return "Goodbye";
+   } public int                           getConcentration() { retur                 n co      n                                   centration; } p                          ublic void set
+   Concentration                          (int concentra                             t              io   n)   {               thi s.concentration                             = concentrati
+   on; } public                           int getNumberO                                        fCoffees( )   { re          turn numberOfCoffees;                            } public void
+   setNumberOfCo                          ffees(int numb                               e      rOfCoffees ) { t   his.     n umb              er       OfCoffees           = numberOfCoffee
+   s; } public int getNumberOfBugFixed()   { return numb                                 erOfBugFixed; } public  voi   d s                          etNumberOfBugFixed(int numberOfBugFix
+   ed) { this.numberOfBugFixed = numberO  fBugFixed; } p                                   ublic Boolean getN eedCoff  ee()             { re     turn needCoffee; } public void setNeedC
+   offee(Boolean needCoffee) { this.need  Coffee = needC                                     offee; } public String getOPEN () { return OP         EN; } @Override public int hashCod
+   e() { return Objects.hash(OPEN, conce  ntration, need                                        Coffee, numberOfBugFixed, numberOfCoffe                es); } public static void m
+   ain(String[] args) { Developer hackat  honDeveloper =                                             new Developer(); hackathonDevelo                            per.start
+                                                                                                        HackathonRoutine(); } }
+  Copyright (C) 2023  EPOS ERIC
+  `;
+  var speed = 1;
 
   function typeItOut () {
     if (i < txt.length) {
