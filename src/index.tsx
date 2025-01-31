3d33a08865a837e72b131da81b3c66e72c1bc0f5
abdelkareem.jebreel <abdelkareem.jebreel@epos-eric.eu>
2025-01-31 18:40:34 +0100

    hashRouter added in order to fix the routing problem

--- src/index.tsx
+++ src/index.tsx
@@ -6,24 +6,11 @@ import "./App.css"; // Import your custom CSS file
 import Router from "./router";
 
 const App: React.FC = () => {
-  // const [isFakeDark, setIsFakeDark] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   document.documentElement.classList.toggle("fake-dark-mode", isFakeDark);
-  // }, [isFakeDark]);
-
   return (
     <div>
       <HashRouter>
           <Router />
       </HashRouter>
-
-      {/* <button
-        onClick={() => setIsFakeDark((prev) => !prev)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button> */}
     </div>
   );
 };
